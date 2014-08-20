5bc1e2a46056b4dad5a7bf468425016395fe678c
sergey-podolsky <sergey.podolsky@gmail.com>
2014-08-20 23:00:56 +0300

    Add tests project

---
+++ XPathBasedDeserializer.Tests/XmlDeserializerTests.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="XmlDeserializerTests.cs" company="Sergey Podolsky">
+//   Sergey Podolsky
+// </copyright>
+// <summary>
+//   Defines the XmlDeserializerTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace XPathBasedDeserializer.Tests
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Contains <see cref="XmlDeserializer"/> tests
+    /// </summary>
+    [TestClass]
+    public class XmlDeserializerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
