b44eff8ac7bc5e260d2216787f29e5e311c8bc8f
sergey-podolsky <sergey.podolsky@gmail.com>
2014-08-20 22:22:59 +0300

    Project settings and assembly info changed

--- XPathBasedDeserializer/Properties/AssemblyInfo.cs
+++ XPathBasedDeserializer/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XPathBasedDeserializer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("XML Deserializer Based on XPath")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sergey Podolsky")]
 [assembly: AssemblyProduct("XPathBasedDeserializer")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
---
+++ XPathBasedDeserializer/XmlDeserializer.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="XmlDeserializer.cs" company="Sergey Podolsky">
+//   Sergey Podolsky
+// </copyright>
+// <summary>
+//   Defines the XmlDeserializer type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace XPathBasedDeserializer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Deserializes objects from XML documents.
+    /// </summary>
+    public class XmlDeserializer
+    {
+    }
+}
