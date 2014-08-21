b492ef113d955810d4b2a8635a644c1f579eb2a6
sergey-podolsky <sergey.podolsky@gmail.com>
2014-08-22 00:46:23 +0300

    Add TypeTree

---
+++ XPathBasedDeserializer.Tests/TypeTreeTests.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeTreeTests.cs" company="Sergey Podolsky">
+//   Copyright (c) Sergey Podolsky. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the TypeTreeTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace XPathBasedDeserializer.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TypeTreeTests
+    {
+        [TestMethod]
+        public void AddsTypeToTree()
+        {
+        }
+    }
+}
---
+++ XPathBasedDeserializer/TypeTree.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeTree.cs" company="Sergey Podolsky">
+//   Copyright (c) Sergey Podolsky. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the TypeTree type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace XPathBasedDeserializer
+{
+    using System;
+
+    /// <summary>
+    /// Contains type inheritance hierarchy
+    /// </summary>
+    /// <typeparam name="T">Type of the data associated data with each type in the hierarchy</typeparam>
+    internal class TypeTree<T>
+    {
+        /// <summary>
+        /// Adds given type with associated data into the type hierarchy
+        /// </summary>
+        /// <param name="type">Type to insert</param>
+        /// <param name="data">Associated data</param>
+        public void Add(Type type, T data)
+        {
+        }
+
+        /// <summary>
+        /// Gets data associated with given type or with the nearest parent type in the hierarchy
+        /// </summary>
+        /// <param name="type">Type of the hierarchy</param>
+        /// <returns>Associated data</returns>
+        public T GetNearestParent(Type type)
+        {
+            return default(T);
+        }
+    }
+}
